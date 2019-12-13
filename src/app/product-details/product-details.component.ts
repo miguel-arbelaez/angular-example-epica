c6db417752432cf7acbe47c91dde81f583e566fb
miguel-arbelaez <miguel@epica.ai>
2019-12-13 12:20:12 -0500

    3 files modified, 7 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,10 +11,16 @@ import { products } from '../products';
 })
 export class ProductDetailsComponent implements OnInit {
 
+  addToCart(product) {
+    window.alert('Your product has been added to the cart!');
+    this.cartService.addToCart(product);
+  }
+
   product;
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
